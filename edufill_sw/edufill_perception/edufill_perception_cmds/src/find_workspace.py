77c5468f9d19e39887aeab567632c9aad8437ee1
Nemo Giftsun <nirmalgibbs@gmail.com>
2013-01-07 10:17:15 +0100

    perception command to extract planes and find the workspace

--- edufill_sw/edufill_perception/edufill_perception_cmds/src/find_workspace.py
+++ edufill_sw/edufill_perception/edufill_perception_cmds/src/find_workspace.py
@@ -6,50 +6,41 @@ import edufill_srvs.srv
 import hbrs_srvs.srv
 
 # Move arm to a cartesian position
-def detect_objects():
-    '''
-    object_finder_srv_start = rospy.ServiceProxy('/edufill_perception/object_segmentation/start',std_srvs.srv.Empty)    
-    rospy.wait_for_service('/edufill_perception/object_segmentation/start', 30)
-
-    try: 
-        response = object_finder_srv_start()
-    except Exception, e:
-        rospy.logerr("service call <<%s>> failed: %s", object_finder_srv_start, e)  
-        return 'srv_call_failed'
-    '''
-    object_finder_srv = rospy.ServiceProxy('/edufill_perception/object_segmentation/get_segmented_objects',hbrs_srvs.srv.GetObjects)    
-    rospy.wait_for_service('/edufill_perception/object_segmentation/get_segmented_objects', 30)
+def find_workspace_cmd():
+    plane_extractor_srv = rospy.ServiceProxy('/find_workspace',edufill_srvs.srv.FindWorkspace)    
+    rospy.wait_for_service('/extract_planes', 30)
 
     rospy.sleep(3)
 
     for i in range(20): 
         print "find object try: ", i
-        resp = object_finder_srv()
+        resp = plane_extractor_srv()
           
-        if (len(resp.objects) <= 0):
-            rospy.loginfo('found no objects')
+        if (len(resp.planarpolygons) <= 0):
+            rospy.loginfo('found no planes')
             rospy.sleep(0.1);
         else:    
-            rospy.loginfo('found {0} objects'.format(len(resp.objects)))
+            rospy.loginfo('found {0} planes'.format(len(resp.planarpolygons)))
             break
         
-    if (len(resp.objects) <= 0):
-        rospy.logerr("no graspable objects found");
-        object_list = []            
+    if (len(resp.planarpolygons) <= 0):
+        rospy.logerr("no planes found");
+        plane_list = []            
         return 'failed'   
     else:
         for obj in resp.objects:
             if obj.pose.pose.position.z <=0.5 and obj.pose.pose.position.z >=30:
                 continue
 
-            object_list = obj
+            workspace = obj
 
-            return object_list
+            return workspace
+
 
 
 if __name__ == '__main__':
-    rospy.init_node('detect_objects')
-    obj_list = detect_objects()
-    print obj_list.pose
+    rospy.init_node('find_workspace')
+    workspace = find_worspace()
+    print workspace
 
 
