eade8a62b4c3b6279863d78b23f18d89607348a9
Nemo Giftsun <nirmalgibbs@gmail.com>
2013-01-18 12:52:15 +0100

    removed as it is not necessary right now

--- edufill_sw/edufill_navigation/edufill_baseplacement/src/edufill_baseplacement/msg/__init__.py
+++
@@ -1,7 +0,0 @@
-from ._OrientToBaseActionFeedback import *
-from ._OrientToBaseActionGoal import *
-from ._OrientToBaseAction import *
-from ._OrientToBaseGoal import *
-from ._OrientToBaseFeedback import *
-from ._OrientToBaseActionResult import *
-from ._OrientToBaseResult import *
