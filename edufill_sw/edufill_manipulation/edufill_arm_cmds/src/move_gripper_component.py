bcab4a6b24aafe093103d20fcf0b42743c1e447e
Nemo Giftsun <nirmalgibbs@gmail.com>
2013-02-23 17:31:53 +0100

    fixed a typo

--- edufill_sw/edufill_manipulation/edufill_arm_cmds/src/move_gripper_component.py
+++ edufill_sw/edufill_manipulation/edufill_arm_cmds/src/move_gripper_component.py
@@ -5,8 +5,8 @@ import roslib
 roslib.load_manifest('edufill_arm_cmds')
 from brics_actuator.msg import JointPositions, JointValue, Poison
 
-#Open or close the gripper
-def move(command):
+#open or close the gripper
+def to_pose(command):
     if isinstance(command,str):
         if command == "OPEN":
 	    gripper_value = 0.0115  
@@ -14,23 +14,24 @@ def move(command):
 	    gripper_value = 0
         else:
             return 'wrong_command'
-    else:
-        # range : 0 - 0.0115 metres  
-        gripper_value = command
+        to_joint_positions([gripper_value,gripper_value])
 
+#control the gripper by gripper positions
+def to_joint_positions(gripper_positions):
+
+    left_gripper_val = gripper_positions[0]
+    right_gripper_val = gripper_positions[1]
     pub = rospy.Publisher("/arm_1/gripper_controller/position_command", JointPositions)
-
     rospy.sleep(0.5)
-
     try:
         jp = JointPositions()
 	jv1 = JointValue()
 	jv1.joint_uri = "gripper_finger_joint_l"
-	jv1.value = gripper_value
+	jv1.value = left_gripper_val
 	jv1.unit = "m"
 	jv2 = JointValue()
 	jv2.joint_uri = "gripper_finger_joint_r"
- 	jv2.value = gripper_value
+ 	jv2.value = right_gripper_val
 	jv2.unit = "m"
 	p = Poison()
  	jp.poisonStamp = p
@@ -44,7 +45,7 @@ def move(command):
 if __name__ == '__main__':
     # range : 0 - 0.0115 metres  
     rospy.init_node('move_gripper_component')
-    move(0.0075)
+    to_joint_positions([0.0115,0.0115])
     
 
 
