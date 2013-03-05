57cb5b5d391e9983a2000f246180f97430d3284a
Elizaveta <elizaveta.shpieva@smail.inf.h-brs.de>
2013-03-05 11:06:17 +0100

    add laser scan service

--- edufill_sw/edufill_perception/edufill_subscriber/src/read_perception_cmd.py
+++ edufill_sw/edufill_perception/edufill_subscriber/src/read_perception_cmd.py
@@ -16,30 +16,38 @@ from sensor_msgs.msg import *
 
 class youbot_subscriber:
     def __init__(self):
-        # setting up variables for message subscription 
-	self.joint_names = ["arm_joint_1", "arm_joint_2", "arm_joint_3", "arm_joint_4", "arm_joint_5","gripper_finger_joint_l","gripper_finger_joint_r"]
-	self.configuration = [0, 0, 0, 0, 0, 0, 0]
-	self.received_state = False
+    # setting up variables for message subscription 
+        self.joint_names = ["arm_joint_1", "arm_joint_2", "arm_joint_3", "arm_joint_4", "arm_joint_5","gripper_finger_joint_l","gripper_finger_joint_r"]
+        self.configuration = [0, 0, 0, 0, 0, 0, 0]
+        self.received_state = False
         self.odom_data = nav_msgs.msg.Odometry()
         self.arm_joint_positions = nav_msgs.msg.Odometry()
+        self.laser_scan_data = sensor_msgs.msg.LaserScan()
 
         # services for advertising perception messages
         self.read_odom_server = rospy.Service('read_odometry_data', edufill_srvs.srv.ReadOdom, self.read_odometry_data)
-        self.read_arm_joint_position_server = rospy.Service('read_arm_joint_position', edufill_srvs.srv.ReadJointPositions,                  self.read_arm_joint_positions)
+        self.read_arm_joint_position_server = rospy.Service('read_arm_joint_position', edufill_srvs.srv.ReadJointPositions,self.read_arm_joint_positions)
+        self.read_laser_scan_server = rospy.Service('read_laser_scan_data', edufill_srvs.srv.ReadLaserScan, self.read_laser_scan_data)
 
-        # subscribers
+
+        # subscribers sensor_msgs/LaserScan
         self.jointstate_sub = rospy.Subscriber('/joint_states', sensor_msgs.msg.JointState, self.joint_states_callback)
         self.odometry_sub = rospy.Subscriber('/odom', nav_msgs.msg.Odometry, self.odom_callback)
+        self.laser_data_sub = rospy.Subscriber('/scan_front', sensor_msgs.msg.LaserScan, self.laser_scan_callback)
 
     def joint_states_callback(self,msg):
         for k in range(7):
             for i in range(len(msg.name)):
-	        joint_name = self.joint_names[k]
-		if(msg.name[i] == joint_name):
-		    self.configuration[k] = msg.position[i]
-		    self.received_state = True
+                joint_name = self.joint_names[k]
+        if(msg.name[i] == joint_name):
+            self.configuration[k] = msg.position[i]
+            self.received_state = True
     def odom_callback(self,msg):
         self.odom_data = msg
+
+    def laser_scan_callback(self,msg):
+        self.laser_scan_data = msg
+
 
     def read_arm_joint_positions(self,req):
         print 'yes'
@@ -47,7 +55,9 @@ class youbot_subscriber:
 
     def read_odometry_data(self,req):
         return edufill_srvs.srv.ReadOdomResponse(self.odom_data)
-    
+
+    def read_laser_scan_data(self,req):
+        return edufill_srvs.srv.ReadLaserScanResponse(self.laser_scan_data)
       
 
 
