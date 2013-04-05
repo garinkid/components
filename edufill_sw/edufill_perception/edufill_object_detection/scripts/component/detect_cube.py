b27c157a849f3f85b276a281e9987c7dae2d5efb
aozhigov <alexei.ozhigov@gmail.com>
2013-04-05 14:31:37 +0200

    Added client for cube detection service.

---
+++ edufill_sw/edufill_perception/edufill_object_detection/scripts/component/detect_cube.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import roslib; roslib.load_manifest('edufill_object_detection')
+import rospy
+from std_msgs.msg import Int64
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import PoseStamped
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+from cv2.cv import CV_FOURCC, RGB
+import numpy as np
+import os
+from os.path import dirname, basename
+from sys import exit, argv, stderr
+from histogram import Histogram
+from myutils import calc_back_proj, draw_cross, hsv_filter_mask, draw_debug_messages
+from numpy.random import randint
+from edufill_object_detection.srv import *
+
+def detect_cube(color, min_size = 10, max_size = 100):
+    cube_detector_srv = rospy.ServiceProxy('/edufill_objdetector/detect_cube', DetectCube) 
+    print "wait for service: /edufill_objdetector/detect_cube"   
+    rospy.wait_for_service('/edufill_objdetector/detect_cube', 30)
+    # call base placement service
+    req = DetectCubeRequest()
+    req.color = color
+    req.min_size = min_size
+    req.max_size = max_size
+    response = cube_detector_srv(req)
+    return response
+
+if __name__ == '__main__':
+    print detect_cube('red')
