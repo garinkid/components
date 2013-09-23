e5c89fa582032c437c2dcc4694a07cbd99d67b32
nemogiftsun <nirmalgibbs@gmail.com>
2013-09-24 01:21:54 +0530

    updated license statments for manipulation components and scripts

--- edufill_sw/edufill_manipulation/edufill_arm_cmds/src/arm_kinematics.py
+++ edufill_sw/edufill_manipulation/edufill_arm_cmds/src/arm_kinematics.py
@@ -1,4 +1,53 @@
 #!/usr/bin/env python
+#******************************************************************************
+# Copyright (c) 2013
+# All rights reserved.
+#
+# Edufill project 
+# Hochschule Bonn-Rhein-Sieg
+# University of Applied Sciences
+# Computer Science Department
+#
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#
+# Author:
+# Nirmal Giftsun, Elizaveta Shpieva, Alexey Ozhigov
+# 
+# Supervised by:
+# Rhama Dwiputra, Bjoern Kahl
+#
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#
+# This software is published under a dual-license: GNU Lesser General Public
+# License LGPL 2.1 and BSD license. The dual-license implies that users of this
+# code may choose which terms they prefer.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+# * Neither the name of Locomotec nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License LGPL as
+# published by the Free Software Foundation, either version 2.1 of the
+# License, or (at your option) any later version or the BSD license.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License LGPL and the BSD license for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License LGPL and BSD license along with this program.
+#
+#******************************************************************************
 import rospy
 import roslib; roslib.load_manifest('edufill_arm_cmds') 
 from youbot_arm_kinematics_analytical_solution import *
@@ -24,17 +73,23 @@ class KinematicsSolver:
       
 
 	def check_ik_solver_has_solution(self,xyzrpy,reference_frame):
+		'''
 		try:
 			return self.ik_solution.check_ik_solver_has_solution(xyzrpy,reference_frame)
 		except:
 			rospy.logerr("The robot has no requested functionality")
+		'''
+		return self.ik_solution.check_ik_solver_has_solution(xyzrpy,reference_frame)
 
 	def get_ik_solution(self,xyzrpy,reference_frame):
+		'''
 		joint_angles = []
 		try:
 			joint_angles = self.ik_solution.get_ik_solution(xyzrpy,reference_frame)
 		except:
 			rospy.logerr("The robot has no requested functionality/the solver does not have any solution")
+		'''
+		joint_angles = self.ik_solution.get_ik_solution(xyzrpy,reference_frame)
 		return joint_angles
 
 
